84222fa669c8a8e48017dda2adfec1e124202c12
simbankenya <pharesmuruthi@gmail.com>
2022-09-01 12:10:27 +0300

    handling async errors installed express-async-errors set up middleware to handle async errors

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require("express")
 const app = express();
 const ejs = require('ejs');
 const Sequelize = require('sequelize')
+require('express-async-errors');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
+
+const errorHandling = (err, req, res, next) => {
+    res.status(500).json({
+        msg: err.message,
+        success: false
+
+    })
+}
+
+app.use(errorHandling)
 
 require('./routes/post.routes')(app)
 
